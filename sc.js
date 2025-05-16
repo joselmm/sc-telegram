2acd0d3030e4f3bd1b3a70369d3e6b59363aee20
joselmm <95531879+joselmm@users.noreply.github.com>
2025-05-15 19:08:44 -0500

    Se solucionó pequeño bug de referencia

--- sc.js
+++ sc.js
@@ -75,8 +75,8 @@ const question = (prompt) => new Promise(resolve => rl.question(prompt, resolve)
         const messageText = msg.message;
         if(!userNamesFilter.includes(username)) return;
         if ( messageText.toLowerCase().includes("approved") || messageText.toLowerCase().includes("✅") ) {
-            await client.sendMessage(groupWhereToSave, { message: text });
-            console.log("↗️  Reenviado:", text);
+            await client.sendMessage(groupWhereToSave, { message: messageText });
+            console.log("↗️  Reenviado:", messageText);
         }
     }
 })();
