a4f46357396bc1b21d8388f5565b0368e3205ef7
joselmm <josealfredosoraca@gmail.com>
2025-12-10 09:05:26 -0500

    se corrigio la referencia al arechivo a a minar

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = "./miembros-unicos-VentasYPromocionesOnline.json"
+const jsonName = "./unique-ids.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
