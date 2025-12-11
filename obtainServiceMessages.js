f456df21d84150eb0f76c3c14a8e53199dbc2cbd
joselmm <josealfredosoraca@gmail.com>
2025-12-11 13:28:37 -0500

    algunos contactos agregados

--- obtainServiceMessages.js
+++ obtainServiceMessages.js
@@ -28,7 +28,7 @@ const stringSession = new StringSession(process.env.SC_TELEGRAM_SESSION_STRING);
     console.log("Session string:\n", client.session.save());
 
     // Cambia esto por el grupo/canal que quieras analizar
-    const chat = "-447977584";
+    const chat = "https://t.me/StreamingColombia247";
     const entity = await client.getEntity(chat);
 
     let offsetId = 0;
