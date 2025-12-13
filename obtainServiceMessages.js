ae0e3e75bc487d6aa675db348b349d2bec97a6a7
joselmm <josealfredosoraca@gmail.com>
2025-12-13 13:41:14 -0500

    miembros-unicos-sistemagestiondeclientes

--- obtainServiceMessages.js
+++ obtainServiceMessages.js
@@ -28,7 +28,7 @@ const stringSession = new StringSession(process.env.SC_TELEGRAM_SESSION_STRING);
     console.log("Session string:\n", client.session.save());
 
     // Cambia esto por el grupo/canal que quieras analizar
-    const chat = "https://t.me/SupremeClubPlus";
+    const chat = "https://t.me/sistemagestiondeclientes";
     const entity = await client.getEntity(chat);
 
     let offsetId = 0;
