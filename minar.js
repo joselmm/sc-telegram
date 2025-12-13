81a27c004ab850bc8e4c3f80b97249f18adbf206
joselmm <josealfredosoraca@gmail.com>
2025-12-13 13:43:52 -0500

    miembros-sistemagestiondeclientes

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = "./miembros-minar.json"
+const jsonName = "./miembros-unicos.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
