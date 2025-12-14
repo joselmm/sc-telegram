db03252c52876f2a9abf9f8a26050166a7f439ef
joselmm <josealfredosoraca@gmail.com>
2025-12-14 16:57:40 -0500

    se minara senders-ids-vendedoresC.json

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = "./senders-ids-intercambios980.json"
+const jsonName = "./senders-ids-vendedoresC.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
--- obtainSendersIds.js
+++ obtainSendersIds.js
@@ -29,7 +29,7 @@ const stringSession = new StringSession(process.env.SC_TELEGRAM_SESSION_STRING);
     console.log("Session string:\n", client.session.save());
 
     // 🔹 Grupo o canal a analizar
-    const chat = "https://t.me/intercambios980";
+    const chat = "https://t.me/vendedoresC";
     const entity = await client.getEntity(chat);
 
     let offsetId = 0;
