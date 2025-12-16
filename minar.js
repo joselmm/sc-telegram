c132635e3ae97427cb38e824edf7444a1265f197
joselmm <josealfredosoraca@gmail.com>
2025-12-16 07:42:58 -0500

    minar senders-ids-ChatKurama.json

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = "./senders-ids-MANU24HRS77.json"
+const jsonName = "./senders-ids-ChatKurama.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
