9d291c23d003e1fe871e98e50f208479df5d63ea
joselmm <josealfredosoraca@gmail.com>
2025-12-16 12:23:22 -0500

    minar senders-ids-private-group-1001746730183.json

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = "./senders-ids-cc_checker_2022.json"
+const jsonName = "./senders-ids-private-group-1001746730183.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
