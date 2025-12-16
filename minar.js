ebea49fc9737d4c263ee07ab0867235515bc5f46
joselmm <josealfredosoraca@gmail.com>
2025-12-16 10:05:10 -0500

    minar senders-ids-CHECKEREstefany_bot.json

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = "./senders-ids-private-group-1001231376304.json"
+const jsonName = "./senders-ids-CHECKEREstefany_bot.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
