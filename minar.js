36c91465174cc39951d49f789edee04953274480
joselmm <josealfredosoraca@gmail.com>
2025-12-21 20:15:33 -0500

    minar miembros-RemChatChk.json

--- minar.js
+++ minar.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express();
 
 const outFile = './theresults.json';
-const jsonName = process.argv[2] || "./miembros-cracking_Web.json"
+const jsonName = process.argv[2] || "./miembros-RemChatChk.json"
 //import { contacts } from "./contacts-array.js"
 
 app.get("/", (_req, res) => {
