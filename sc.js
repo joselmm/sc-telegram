85edf74af8743065be72e03192a185d72f35709e
joselmm <josealfredosoraca@gmail.com>
2025-12-23 22:39:39 -0500

    minar todo 23 dic 2025

--- sc.js
+++ sc.js
@@ -27,7 +27,7 @@ console.log("API_HASH:", process.env.TELEGRAM_API_HASH);
   console.log("✅ Conectado correctamente");
 
   // Obtiene la entidad del grupo o canal
-  var chat = "https://t.me/BINNEROSCC";
+  var chat = process.argv[2] || "https://t.me/BINNEROSCC";
   const entity = await client.getEntity(chat);
   console.log("Tipo de chat:", JSON.stringify(entity));
 
